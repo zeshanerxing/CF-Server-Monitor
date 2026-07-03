240b6d86e8d3a044728e3e32dd46d3dd57c2fd25
Huilang <admin@huilang.me>
2026-07-03 23:16:44 +0800

    build: a新增GitHub Pages部署工作流，添加build:github-page打包命令

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.VITE_BASE || '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src/frontend')
